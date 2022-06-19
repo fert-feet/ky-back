e906b6fae5c14230b55efd80541668c0c609216b
ke2fy <3214588281@qq.com>
2022-06-20 00:23:47 +0800

    解决跨域问题

---
+++ ky-system/src/main/java/com/ky/mapper/SysUserMapper.java
@@ -0,0 +1,18 @@
+package com.ky.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ky.domain.SysUser;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author KY
+ * @since 2022-06-19
+ */
+@Mapper
+public interface SysUserMapper extends BaseMapper<SysUser> {
+
+}
