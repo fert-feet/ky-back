6143272a4ea43eee73105faf5717979d0d4d979f
ke2fy <3214588281@qq.com>
2022-06-19 23:34:48 +0800

    用户分页功能完成

---
+++ ky-admin/src/main/java/com/ky/web/SysUserController.java
@@ -0,0 +1,69 @@
+package com.ky.web;
+
+import com.ky.dto.sys_user.UpdateUserDto;
+import com.ky.dto.sys_user.UserDto;
+import com.ky.result.ResultVo;
+import com.ky.service.ISysUserService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author KY
+ * @since 2022-06-19
+ */
+@RestController
+@RequestMapping("/user")
+public class SysUserController {
+
+    @Resource
+    private ISysUserService userService;
+
+    /**
+     * 查找所有用户
+     *
+     * @return
+     */
+    @GetMapping("/index")
+    public ResultVo index(@RequestParam int currentNum, @RequestParam int pageSize) {
+        return userService.pageIndex(currentNum, pageSize);
+    }
+
+    /**
+     * 创建用户
+     *
+     * @param user
+     * @return
+     */
+    @PutMapping("/save")
+    public ResultVo save(@RequestBody @Validated UserDto user) {
+        return userService.create(user);
+    }
+
+    /**
+     * 更新用户
+     *
+     * @param user
+     * @return
+     */
+    @PatchMapping("/update")
+    public ResultVo update(@RequestBody @Validated UpdateUserDto user) {
+        return userService.update(user);
+    }
+
+    /**
+     * 删除用户
+     *
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/delete")
+    public ResultVo delete(@RequestParam(value = "id", required = true) int id) {
+        return userService.delete(id);
+    }
+}
